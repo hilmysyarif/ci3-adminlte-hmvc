c38b42a4a7bcfd16a7af0160164a6eaabfb3cf21
suwito <suwito.lt@gmail.com>
2016-11-21 19:47:42 +0700

    Initialize commit

---
+++ application/modules/supplier/language/english/supplier_lang.php
@@ -0,0 +1,38 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+// ============================
+$lang['supplier_title_manage'] 		= 'Supplier';
+$lang['supplier_title_new'] 		= 'Supplier Baru';
+$lang['supplier_title_edit'] 		= 'Edit Supplier';
+
+// form/table
+$lang['supplier_name'] 				= 'Nama Supplier';
+$lang['supplier_addr'] 				= 'Alamat';
+$lang['supplier_telp1'] 			= 'Telp 1';
+$lang['supplier_telp2'] 			= 'Telp 2';
+$lang['supplier_email'] 			= 'Email';
+$lang['supplier_cp'] 				= 'Contact Person';
+$lang['supplier_desc'] 				= 'Keterangan';
+$lang['supplier_status'] 			= 'Status';
+$lang['supplier_active'] 			= 'Aktif';
+$lang['supplier_inactive'] 			= 'Tidak Aktif';
+
+
+// button
+$lang['supplier_btn_new'] 			= 'Baru';
+$lang['supplier_btn_delete'] 		= 'Hapus';
+$lang['supplier_btn_save'] 			= 'Simpan';
+$lang['supplier_btn_cancel'] 		= 'Batal';
+
+// messages
+$lang['supplier_del_error']			= 'Anda belum memilih supplier yang akan dihapus.';
+$lang['supplier_del_failure']		= 'Tidak dapat menghapus supplier: ';
+$lang['supplier_delete_confirm']	= 'Apakah anda yakin akan menghapus supplier terpilih ?';
+$lang['supplier_deleted']			= 'Supplier terhapus';
+$lang['supplier_no_records_found'] 	= 'Data tidak ditemukan.';
+
+$lang['supplier_create_failure'] 	= 'Supplier baru gagal disimpan: ';
+$lang['supplier_create_success'] 	= 'Supplier baru berhasil disimpan';
+
+$lang['supplier_edit_success'] 		= 'Supplier berhasil disimpan';
+$lang['supplier_invalid_id'] 		= 'ID Tidak Valid';
