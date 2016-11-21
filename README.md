0ffcf51c09f0cd287d6964087549c774675ed610
suwito <suwito.lt@gmail.com>
2016-11-21 21:53:34 +0700

    Update language and README.md

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Base of Web Application using Codeigniter 3, HMVC, and AdminLTE Theme
 1. Import DB on "ci3-adminlte-hmvc\DB"
 2. Change Config on "ci3-adminlte-hmvc\application\config" base on your server.
 3. Hope helpful :)
+
+#Donation
+<a href='https://pledgie.com/campaigns/32930'><img alt='Click here to lend your support to: Codeigniter 3 + AdminLTE + HMVC and make a donation at pledgie.com !' src='https://pledgie.com/campaigns/32930.png?skin_name=chrome' border='0' ></a>
--- application/modules/supplier/language/english/supplier_lang.php
+++ application/modules/supplier/language/english/supplier_lang.php
@@ -2,37 +2,37 @@
 
 // ============================
 $lang['supplier_title_manage'] 		= 'Supplier';
-$lang['supplier_title_new'] 		= 'Supplier Baru';
+$lang['supplier_title_new'] 		= 'New Supplier';
 $lang['supplier_title_edit'] 		= 'Edit Supplier';
 
 // form/table
-$lang['supplier_name'] 				= 'Nama Supplier';
-$lang['supplier_addr'] 				= 'Alamat';
-$lang['supplier_telp1'] 			= 'Telp 1';
-$lang['supplier_telp2'] 			= 'Telp 2';
+$lang['supplier_name'] 				= 'Supplier Name';
+$lang['supplier_addr'] 				= 'Address';
+$lang['supplier_telp1'] 			= 'Phone 1';
+$lang['supplier_telp2'] 			= 'Phone 2';
 $lang['supplier_email'] 			= 'Email';
 $lang['supplier_cp'] 				= 'Contact Person';
-$lang['supplier_desc'] 				= 'Keterangan';
+$lang['supplier_desc'] 				= 'Description';
 $lang['supplier_status'] 			= 'Status';
-$lang['supplier_active'] 			= 'Aktif';
-$lang['supplier_inactive'] 			= 'Tidak Aktif';
+$lang['supplier_active'] 			= 'Active';
+$lang['supplier_inactive'] 			= 'Inactive';
 
 
 // button
-$lang['supplier_btn_new'] 			= 'Baru';
-$lang['supplier_btn_delete'] 		= 'Hapus';
-$lang['supplier_btn_save'] 			= 'Simpan';
-$lang['supplier_btn_cancel'] 		= 'Batal';
+$lang['supplier_btn_new'] 			= 'New';
+$lang['supplier_btn_delete'] 		= 'Delete';
+$lang['supplier_btn_save'] 			= 'Save';
+$lang['supplier_btn_cancel'] 		= 'Cancel';
 
 // messages
-$lang['supplier_del_error']			= 'Anda belum memilih supplier yang akan dihapus.';
-$lang['supplier_del_failure']		= 'Tidak dapat menghapus supplier: ';
-$lang['supplier_delete_confirm']	= 'Apakah anda yakin akan menghapus supplier terpilih ?';
-$lang['supplier_deleted']			= 'Supplier terhapus';
-$lang['supplier_no_records_found'] 	= 'Data tidak ditemukan.';
+$lang['supplier_del_error']			= 'You have not been select the supplier that will be deleted';
+$lang['supplier_del_failure']		= 'Can not delete supplier : ';
+$lang['supplier_delete_confirm']	= 'Are you sure to delete selected supplier ?';
+$lang['supplier_deleted']			= 'Supplier deleted';
+$lang['supplier_no_records_found'] 	= 'Data not found.';
 
-$lang['supplier_create_failure'] 	= 'Supplier baru gagal disimpan: ';
-$lang['supplier_create_success'] 	= 'Supplier baru berhasil disimpan';
+$lang['supplier_create_failure'] 	= 'New supplier saving failed : ';
+$lang['supplier_create_success'] 	= 'New supplier saved successfully';
 
-$lang['supplier_edit_success'] 		= 'Supplier berhasil disimpan';
-$lang['supplier_invalid_id'] 		= 'ID Tidak Valid';
+$lang['supplier_edit_success'] 		= 'Supplier saved successfully';
+$lang['supplier_invalid_id'] 		= 'Invalid ID';
